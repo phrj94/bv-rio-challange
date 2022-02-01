400d9cae6bb9c1fdee4e1c4b82073f8aab0e629f
phrj94 <ph.rj.94@hotmail.com>
2022-02-01 17:07:06 -0300

    Ajustando aplicação pra ir direto ao uso da funcionalidade necessária

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,9 @@
 import { createWebHistory, createRouter } from "vue-router";
-import Home from "../components/Home.vue";
 import WeatherForecast from "../components/WeatherForecast.vue";
 const baseURL = process.env.NODE_ENV === "production" ? "/bv-rio/" : "/";
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/forecast-weather",
     name: "WeatherForecast",
     component: WeatherForecast,
   },
